950e70144ddecbe235230d26d2371229d7ba40f6
Renan G. Dutra <r.granatodutra@gmail.com>
2023-06-04 01:17:40 -0300

    feat:  created player related methods in maze

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from maze import Maze
+from player import Player
 from mocked_mazes import test_maze_1
 
 labirinto = Maze.create_from_dict(test_maze_1)
+jogador = Player("Renan", "♫")
+
+labirinto.spawn_player(jogador, 3, 3)
+labirinto.show()
+
+print(jogador)
+jogador.set_goal(9,5)
+print("")
+jogador.show_goal(str_mode=True)
 print(labirinto)
--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 class Maze:
-    def __init__(self, width: int, height: int):
+    def __init__(self, name: str, width: int, height: int):
+        self.name = name
         self.table = [[" " for _ in range(width)] for _ in range(height)]
         self.table_height = height
         self.table_width = width
@@ -11,6 +12,7 @@ class Maze:
         return maze_str
 
     def add_wall(self, start_pos_x: int, start_pos_y: int, wall_width: int, wall_height: int, alias: str="W"):
+        """ Função que adiciona uma parede ao labirinto """
         possible_width = (self.table_width - start_pos_x - wall_width) >= 0
 
         if not possible_width:
@@ -28,20 +30,64 @@ class Maze:
         
         current_pos_x = start_pos_x 
 
-        for x in range(wall_width):
+        for _ in range(wall_width):
             current_pos_y = start_pos_y
-            for y in range(wall_height):
+            for _ in range(wall_height):
                 self.table[current_pos_y][current_pos_x] = alias
                 current_pos_y += 1 
             current_pos_x +=1
     
+    def spawn_player(self, player, pos_x: int, pos_y: int):
+        """ Função que spawna um player dentro do labirinto """
+        if (pos_x + 1) > self.table_width or (pos_y + 1) > self.table_height:
+            return print('Invalid position.')
+        if self.table[pos_y][pos_x] != " ":
+            return print('This position is already occupied by something.')
+        self.table[pos_y][pos_x] = player.alias
+
+        player.set_maze(self, pos_x, pos_y)
+
+        return print(f'Player spawned on x: {pos_x}, y: {pos_y}')
+    
+    def check_position(self, x: int, y: int):
+        """ Função que checa se uma posição do labirinto está ocupada """
+        if x > self.table_width or y > self.table_height:
+            return "invalid"
+        if self.table[y][x] != " ":
+            return "blocked"
+        return "valid"
+    
+    
+    def show_position(self, x: int, y: int, str_mode):
+        """ Função que exibe com uma marcação, uma posição do labirinto se a mesma estiver livre """
+        checked_position = self.check_position(x, y)
+
+        if not checked_position == "valid":
+            return print("This position is " + checked_position + "...")
+        
+        table_copy = [row[:] for row in self.table]
+        table_copy[y][x] = "✪"
+
+        if not str_mode:
+            for row in table_copy:
+                print(row)
+        else:
+            maze_str = ""
+            for row in table_copy:
+                maze_str += ' '.join(row) + '\n'
+            return print(maze_str)
+
     def show(self):
+        """ Função que mostra labirinto em formato de array """
+        print(" ")
         for row in self.table:
             print(row)
+        print(" ")
     
     @staticmethod
     def create_from_dict(maze_dict: dict):
-        new_maze = Maze(maze_dict["width"], maze_dict["height"])
+        """ Função que cria labirinto a partir de um dicionário (mocked_mazes) """
+        new_maze = Maze(maze_dict["name"], maze_dict["width"], maze_dict["height"])
         for wall in maze_dict["walls"]:
             new_maze.add_wall(wall["sx"], wall["sy"], wall["w"], wall["h"], wall["a"])
         return new_maze
